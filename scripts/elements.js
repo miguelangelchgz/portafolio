ff5ac6aaf01dd18d99f4f4839ac49bf80f19a0b0
miguelangelchgz <miguelangelchgz@gmail.com>
2019-01-10 11:34:52 -0400

    first commit

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,5 +1,8 @@
-const HTMLprojectCard = '<div class="col-md-4  mb-3"><div class="card"><img class="card-img-top" src="%projectImage%" alt="Card image cap"><div class="card-body"><h5 class="card-title">projectTitle</h5><p class="card-text">projectDescription</p><a target="_blank" href="%projectLink%" class="my-btn ">LINK</a></p></div></div>';
-const HTMLheaderTitle = '<h1 class="h1 font-weight-bold py-1">%data%</h1>';
-const HTMLbuttonAbout = '<div class="col-md-5 col"><a class="my-btn btn-lg btn-block">Sobre mí</a></div>';
-const HTMLbuttonContact = '<div class="col-md-5 col"><a class="btn-lg btn-block my-btn-out">Contacto</a></div>';
-const HTMLparagraph = '<p class="mb-5">%data%</p>';
+'use strict';
+
+var HTMLprojectCard = '<div class="col-md-4  mb-3"><div class="card"><img class="card-img-top" src="%projectImage%" alt="Card image cap"><div class="card-body"><h5 class="card-title">projectTitle</h5><p class="card-text">projectDescription</p><a target="_blank" href="%projectLink%" class="my-btn ">LINK</a></p></div></div>';
+var HTMLheaderTitle = '<h1 class="h1 font-weight-bold py-1">%data%</h1>';
+var HTMLbuttonAbout = '<div class="col-md-5 col"><a class="my-btn btn-lg btn-block">Sobre mí</a></div>';
+var HTMLbuttonContact = '<div class="col-md-5 col"><a class="btn-lg btn-block my-btn-out">Contacto</a></div>';
+var HTMLparagraph = '<p class="mb-5">%data%</p>';
+//# sourceMappingURL=elements.js.map
